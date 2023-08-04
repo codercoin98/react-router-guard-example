66995044393ee915b3178ac8ba49115d83515aa2
chenbin.li <1015761882@qq.com>
2023-08-04 09:40:27 +0800

    chore: add some comments

--- src/router/AuthRouter.tsx
+++ src/router/AuthRouter.tsx
@@ -7,10 +7,12 @@ interface Props {
 const AuthRouter = (props: Props) => {
   const { auth = false, children } = props;
   if (auth) {
-    console.log(1);
-
-    //Protected Route, check Authentication of user
+    // Protected Route, check Authentication of user
+    // 1. Check both role and token of user.
+    // 2. Invalid authentication, the component will be redirected to the login page.
+    // 3. Valid Authentication , the component will return the content of the router.
     const token = localStorage.getItem('token');
+    // if(checkRoleAuth(user.role)){......}
     if (token === null || token === '') {
       return <Navigate to="/login" replace />;
     }
