4a9dfc6455343a07dd7d39af782dc7dc789462d7
chenbin.li <1015761882@qq.com>
2023-08-10 10:22:23 +0800

    chore: disable automatic startup of the dev server

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: false
+  },
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
   },
